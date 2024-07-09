4c72457853bb02bf226a88046ce2e3d995bff9b8
RobertaNer3s <robertha07rocha@gmail.com>
2024-07-08 23:00:25 -0300

    feat: create info modal

---
+++ src/app/components/modal/modal-more-information-dog/modal-more-information-dog.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Dog } from '../../../models/Dog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-modal-more-information-dog',
+  standalone: true,
+  imports: [
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+  ],
+  templateUrl: './modal-more-information-dog.component.html',
+  styleUrl: './modal-more-information-dog.component.scss',
+})
+export class ModalMoreInformationDogComponent {
+  @Output() closeModal = new EventEmitter();
+  @Input() dog: Dog | null = null;
+  readonly dialogRef = inject(MatDialogRef<ModalMoreInformationDogComponent>);
+  readonly data = inject<{dog: Dog}>(MAT_DIALOG_DATA);
+
+  public onClose(): void {
+    this.dialogRef.close();
+  }
+
+  public handleCloseModal(): void {
+    this.closeModal.emit(false);
+  }
+}
